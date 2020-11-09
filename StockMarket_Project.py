d10f757219dd9fb55907fc0adeddc2a56c59dc68
Batsy003 <67663379+Batsy003@users.noreply.github.com>
2020-11-09 10:32:09 -0500

    Update StockMarket_Project.py

--- StockMarket_Project.py
+++ StockMarket_Project.py
@@ -112,12 +112,13 @@ def IndivualPrice(stockD, stockP):
         dateInput = str(input("Either you entered a date that falls on a weekend or its not in the format yyyy-mm-dd" +"\n" "Please re-enter the date: "))
 
 def highLow(stockP, start, end):
+    print (start, end)
     stockSort = []
 
     for i in range(start, end):
         stockSort.append(stockP[i])
 
-    for i in range(start, end):
+    for i in range(0, len(stockSort) - 1):
         temp = stockSort[i]
         temp2 = i - 1
         while (temp2 >=0) and (float(stockSort[temp2]) > float(temp)):
